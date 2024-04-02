f41db5b65f6a717e9a4a2e7c9d50c3f75b93db86
markostupar98 <markostuparets@gmail.com>
2024-04-02 19:04:33 +0200

    Adding drag and drop funcionality

--- app/(platform)/(dashboard)/board/[boardId]/_components/cardForm.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/cardForm.tsx
@@ -1,5 +1,17 @@
 "use client";
 
+import { useRef, ElementRef, KeyboardEventHandler, forwardRef } from "react";
+import { Plus, X } from "lucide-react";
+
+import { FormSubmitButton } from "@/components/form/formSubmitButton";
+import { FormTextarea } from "@/components/form/formTextarea";
+import { Button } from "@/components/ui/button";
+import { useOnClickOutside, useEventListener } from "usehooks-ts";
+import { useParams } from "next/navigation";
+import { useAction } from "@/hooks/useAction";
+import { createCard } from "@/services/createCard";
+import { toast } from "sonner";
+
 interface CardFormProps {
   listId: string;
   enableEditing: () => void;
@@ -7,11 +19,79 @@ interface CardFormProps {
   isEditing: boolean;
 }
 
-export const CardForm = ({
-  listId,
-  enableEditing,
-  disableEditing,
-  isEditing,
-}: CardFormProps) => {
-  return <div>Card form</div>;
-};
+export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
+  ({ listId, enableEditing, disableEditing, isEditing }, ref) => {
+    const params = useParams();
+    const formRef = useRef<ElementRef<"form">>(null);
+
+    const { execute, fieldErrors } = useAction(createCard, {
+      onSuccess: (data) => {
+        toast.success(`Card ${data.title} created succesfully`);
+        formRef.current?.reset();
+      },
+      onError: (error) => toast.error(error),
+    });
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        disableEditing();
+      }
+    };
+
+    useOnClickOutside(formRef, disableEditing);
+    useEventListener("keydown", onKeyDown);
+
+    const onTextareaKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = (
+      e
+    ) => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        formRef.current?.requestSubmit();
+      }
+    };
+
+    const onSubmit = (formData: FormData) => {
+      const title = formData.get("title") as string;
+      const listId = formData.get("listId") as string;
+      const boardId = params.boardId as string
+
+      execute({ title, listId, boardId });
+    };
+
+    if (isEditing) {
+      return (
+        <form action={onSubmit} ref={formRef} className="m-1 py-0.5 space-y-4">
+          <FormTextarea
+            id="title"
+            onKeyDown={onTextareaKeyDown}
+            ref={ref}
+            placeholder="Enter a card name"
+            errors={fieldErrors}
+          />
+          <input hidden id="listId" name="listId" value={listId} />
+          <div className="flex items-center gap-x-1">
+            <FormSubmitButton>Add card</FormSubmitButton>
+            <Button onClick={disableEditing} size="sm" variant="ghost">
+              <X className="h-5 w-5" />
+            </Button>
+          </div>
+        </form>
+      );
+    }
+    return (
+      <div className="pt-2 px-2">
+        <Button
+          onClick={enableEditing}
+          className="h-auto px-2 py-1.5 w-full justify-start text-muted-foreground text-sm"
+          size="sm"
+          variant="ghost"
+        >
+          <Plus className="h-4 w-4 mr-2" />
+          Add a card
+        </Button>
+      </div>
+    );
+  }
+);
+
+CardForm.displayName = "CardForm";
--- app/(platform)/(dashboard)/board/[boardId]/_components/listContainer.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/listContainer.tsx
@@ -1,30 +1,144 @@
 "use client";
 
 import { ListWithCards } from "@/types";
-import { List } from "@prisma/client";
+import { useAction } from "@/hooks/useAction";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 import { ListForm } from "./listForm";
 import { useEffect, useState } from "react";
 import { ListItem } from "./listItem";
+import { updateListOrder } from "@/services/updateListOrder";
+import { toast } from "sonner";
 
 interface ListContainerProps {
   data: ListWithCards[];
   boardId: string;
 }
 
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+
+  result.splice(endIndex, 0, removed);
+
+  return result;
+}
+
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
   const [orderedData, setOrderedData] = useState(data);
+
+  const { execute: executeUpdateListOrder } = useAction(updateListOrder, {
+    onSuccess: () => toast.success('List reordered'),
+    onError:(error) => toast.error(error)
+  });
 
   useEffect(() => {
     setOrderedData(data);
   }, [data]);
 
+  const onDragEnd = (result: any) => {
+    const { destination, source, type } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    // Dropped in the same position as it was before
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    // If user moves a list
+    if (type === "list") {
+      const items = reorder(orderedData, source.index, destination.index).map(
+        (item, index) => ({ ...item, order: index })
+      );
+      setOrderedData(items);
+      executeUpdateListOrder({items, boardId})
+    }
+
+    // If user moves a card
+    if (type === "card") {
+      let newOrderedData = [...orderedData];
+
+      // Source and destination list
+      const sourceList = newOrderedData.find(
+        (list) => list.id === source.droppableId
+      );
+
+      const destinationList = newOrderedData.find(
+        (list) => list.id === destination.droppableId
+      );
+
+      if (!sourceList || !destinationList) {
+        return;
+      }
+      if (!sourceList.cards) {
+        sourceList.cards = [];
+      }
+      if (!destinationList.cards) {
+        destinationList.cards = [];
+      }
+      if (source.droppableId === destination.droppableId) {
+        const reorderedCards = reorder(
+          sourceList.cards,
+          source.index,
+          destination.index
+        );
+
+        reorderedCards.forEach((card, index) => {
+          card.order = index;
+        });
+
+        sourceList.cards = reorderedCards;
+
+        setOrderedData(newOrderedData);
+      }
+      // User moves a card to another list
+      else {
+        const [movedCard] = sourceList.cards.splice(source.index, 1);
+
+        // Assining new list id to the moved card
+        movedCard.listId = destination.droppableId;
+
+        // Adding card to the destination list
+        destinationList.cards.splice(destination.index, 0, movedCard);
+
+        sourceList.cards.forEach((card, index) => {
+          card.order = index;
+        });
+        setOrderedData(newOrderedData);
+      }
+    }
+  };
+
   return (
-    <ol className="flex gap-x-3 h-full">
-      {orderedData.map((list, index) => {
-        return <ListItem id={list.id} key={list.id} index={index} data={list} />;
-      })}
-      <div className="flex-shrink-0 w-1" />
-      <ListForm />
-    </ol>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="lists" type="list" direction="horizontal">
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-x-3 h-full"
+          >
+            {orderedData.map((list, index) => {
+              return (
+                <ListItem
+                  id={list.id}
+                  key={list.id}
+                  index={index}
+                  data={list}
+                />
+              );
+            })}
+            {provided.placeholder}
+            <div className="flex-shrink-0 w-1" />
+            <ListForm />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
